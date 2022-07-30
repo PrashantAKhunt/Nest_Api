a02c7b681c020b55f8deb60590f087241427ebf2
Prashant Khunt <prashant@atliq.com>
2022-07-30 15:03:10 +0530

    auth module completed

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { GetUser } from 'src/auth/decorator';
+import { JwtGuard } from 'src/auth/guard';
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
 
-    @UseGuards(AuthGuard('jwt'))
     @Get('me')
-    getMe(){
-        return 'Hello World!';
+    getMe(@GetUser() user: User){
+
+        console.log({
+            user: user,
+        });
+
+        
+        return user;
     }
 }

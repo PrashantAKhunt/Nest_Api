22f35a3d19094c3e7095de75108ae9edda30f219
Prashant Khunt <prashant@atliq.com>
2022-07-30 11:17:40 +0530

    access_token send at the time of login and registration

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,8 @@ import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
 import * as argon from "argon2";
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
+import { JwtService } from "@nestjs/jwt";
+import { ConfigService } from "@nestjs/config";
 
 
 @Injectable({
@@ -11,9 +13,11 @@ import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 
 export class AuthService{
 
-    constructor(private prisma:PrismaService){
-
-    }
+    constructor(
+        private prisma:PrismaService,
+        private jwt:JwtService,
+        private config:ConfigService
+    ){}
 
     async signup(dto: AuthDto){ 
 
@@ -28,9 +32,7 @@ export class AuthService{
                     hash
                 }
             })
-
-            delete user.hash;
-            return user;
+            return this.signToken(user.id,user.email);
         }
         catch(error)
         {
@@ -66,9 +68,24 @@ export class AuthService{
             throw new ForbiddenException("credentials are invalid");
         }
 
-        delete user.hash;
-
-        return user;
+        return this.signToken(user.id,user.email);
     }
 
+    async signToken(userId:number , email:string): Promise<{access_token:string}>{
+        const payload = {
+            sub:userId,
+            email
+        }
+
+        const secret = this.config.get("JWT_SECRET");
+
+        const token = await this.jwt.signAsync(payload,{
+            expiresIn:'15m',
+            secret:secret
+        });
+
+        return {
+            access_token:token
+        }; 
+    }
 }
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
 
-    constructor(){
+    constructor(config: ConfigService){
         super({
             datasources:{
                 db:{
-                    url: 'mysql://root:@localhost:3306/nest_api',
+                    url: config.get('DATABASE_URL'),
                 }
             }
         });
